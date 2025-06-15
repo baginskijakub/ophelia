03cb25f90fd7d38955139f985d119a7478477c25
hedrekao <82450784+Hedrekao@users.noreply.github.com>
2025-06-15 13:57:02 +0200

    add basic logout example

--- apps/platform/src/app/(protected)/[organization]/page.tsx
+++ apps/platform/src/app/(protected)/[organization]/page.tsx
@@ -1,13 +1,21 @@
-import { withAuth } from "@workos-inc/authkit-nextjs";
+import { signOut, withAuth } from "@workos-inc/authkit-nextjs";
 
 const Page = async () => {
   const { user } = await withAuth({ ensureSignedIn: true });
 
   return (
     <>
-      <p>
-        Welcome back {user.firstName} - {user.email}
-      </p>
+      <form
+        action={async () => {
+          'use server';
+          await signOut();
+        }}
+      >
+        <p>
+          Welcome back {user.firstName} - {user.email}
+        </p>
+        <button type="submit">Sign out</button>
+      </form>
     </>
   );
 };
