965899c0efaaee6829be868ad54496a4687416b5
baginskijakub <92220319+baginskijakub@users.noreply.github.com>
2025-08-26 12:40:31 +0200

    textarea, capitalize company

--- packages/utils/src/capitalize.ts
+++ packages/utils/src/capitalize.ts
@@ -1,6 +1,14 @@
-export const capitalize = (str: string): string => {
-  if (typeof str !== 'string' || str.length === 0) {
+export const capitalize = (str: string, allWords?: boolean): string => {
+  if (typeof str !== "string" || str.length === 0) {
     return str;
   }
+
+  if (allWords) {
+    return str
+      .split(" ")
+      .map((word) => capitalize(word))
+      .join(" ");
+  }
+
   return str.charAt(0).toUpperCase() + str.slice(1);
-}
+};
